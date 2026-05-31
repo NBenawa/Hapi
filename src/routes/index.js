73be93c0b2ea5d460236d6cc3059e7c8d85b78bd
Najibullah Mohammadi <najeeb.benawa@gmail.com>
2026-05-31 13:53:47 +0200

    adding routes to the index.js file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,17 @@
 import { addViewToListingRoute } from './addViewToListing';
 import { getAllListingsRoute } from "./getAllListing";
 import { getListingRoute } from './getListing';
+import { getUserListingsRoute } from './getUserListings';
+import { addListingRoute } from './createNewListing';
+import { updateListingRoute } from './updateListing';
+import { deleteListingRoute } from './deleteListing';
 
 export default [
     addViewToListingRoute,
     getAllListingsRoute,
     getListingRoute,
+    getUserListingsRoute,
+    addListingRoute,
+    updateListingRoute,
+    deleteListingRoute,
 ];
